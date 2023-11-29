231037548609d5ba1acf3cf28d62d6cc5cd9efff
AbuRasel9 <aburaselahamed99@gmail.com>
2023-11-29 19:31:22 +0600

    cancelled task get api done and inprogress start

--- lib/screens/cancelled_task_screen.dart
+++ lib/screens/cancelled_task_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:untitled1/models/CancelledTaskModel.dart';
+import 'package:untitled1/network_services/network_requester.dart';
+import 'package:untitled1/utils/urls.dart';
 import 'package:untitled1/wdgets/task_widget.dart';
 
 class CancelledTaskScreen extends StatefulWidget {
@@ -9,18 +12,39 @@ class CancelledTaskScreen extends StatefulWidget {
 }
 
 class _CancelledTaskScreenState extends State<CancelledTaskScreen> {
+  CancelledTaskModel? _cancelledTaskModel;
+  Future cancelledTastFormApi() async {
+    final response=await networkRequester().getRequester(Urls.cancelledTask);
+    if(response['status']=="success"){
+      _cancelledTaskModel=CancelledTaskModel.fromJson(response);
+      setState(() {
+
+      });
+    }
+  }
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      cancelledTastFormApi();
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: 20,
+        itemCount: _cancelledTaskModel?.data?.length ?? 0,
         itemBuilder: (context,index){
+          final task=_cancelledTaskModel!.data![index];
       return   TaskWidget(
-        title: 'This is Title',
-        description: 'This is des',
+        title: task.title ?? "Unknown",
+        description: task.description ?? "unknown",
         type: 'Cancelled',
         onEditTap: () {},
         onDeleteTap: () {},
-        date: '1/02/22',
+        date: task.createdDate ?? "Unknown",
       );
 
     });
--- lib/utils/urls.dart
+++ lib/utils/urls.dart
@@ -3,4 +3,5 @@ class Urls{
   static String newTask="$baseUrl/listTaskByStatus/New";
   static String completedTask="$baseUrl/listTaskByStatus/Completed";
   static String cancelledTask="$baseUrl/listTaskByStatus/Cancelled";
+  static String progressTask="$baseUrl/listTaskByStatus/{Porgress}";
 }
