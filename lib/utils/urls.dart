ee5766309c685c3b9be707e6664232585e2e833c
AbuRasel9 <aburaselahamed99@gmail.com>
2023-12-04 14:58:06 +0600

    update task by status

--- lib/utils/urls.dart
+++ lib/utils/urls.dart
@@ -3,7 +3,8 @@ class Urls{
   static String newTask="$baseUrl/listTaskByStatus/New";
   static String completedTask="$baseUrl/listTaskByStatus/Completed";
   static String cancelledTask="$baseUrl/listTaskByStatus/Cancelled";
-  static String progressTask="https://task.teamrabbil.com/api/v1/listTaskByStatus/Progress";
+  static String progressTask="$baseUrl/listTaskByStatus/Progress";
+  static String updateTask(String taskId,String taskStatus)=>"$baseUrl/updateTaskStatus/$taskId/$taskStatus";
 
 
 
